8ccf8e537798e1e072f44ee03b47532077c81481
hansputera <47862061+hansputera@users.noreply.github.com>
2020-07-04 21:49:18 +0700

    Create class.php

---
+++ class.php
@@ -0,0 +1,43 @@
+<?php
+
+
+class Database {
+  
+ public function get($param) {
+  if (!$param) {
+   return "Harus ada param";
+  }
+  $data = file_get_contents('json/data.json');
+
+  $result = json_encode($data);
+  $findData = $result->$param;
+
+  if (!$findData) {
+   return "Data tidak ada.";
+  } else {
+   return $findData;
+  }
+ }
+
+
+ public function set($key, $value) {
+ if (!$key) {
+  return "Masukan keynya";
+ }
+ if (!$value) {
+  return "Masukan valuenya.";
+ }
+
+ $file = file_get_contents('json/data.json');
+ $result = json_encode($file);
+
+ $findData = $result->$key;
+ if (!$findData) {
+   return "Data tidak ditemukan.";
+ } else {
+   $ubah = $findData
+  }
+ }
+}
+
+?>
